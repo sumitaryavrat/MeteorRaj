46f1308bdd1b666fec8ab07859642bc592eacd77
Sumit Sharma <sumit.sharma@aryavratinfotech.com>
2018-09-13 18:37:01 +0530

    Project complete

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -6,17 +6,15 @@ import ReactDOM from 'react-dom';
 import { Tasks } from '../api/tasks.js';
 import UIWrapper from './UIWrapper.js';
 import Task from './Task.js';
-import Posts from './Posts.js';
 import { Meteor } from 'meteor/meteor';
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { sunburst } from "react-syntax-highlighter/dist/styles/hljs";
-//import MyList from './Demo'
-// App component - represents the whole app
-class App extends Component {
-  constructor(props) {
-    super(props);
- 
-    this.state = {
+import "bootstrap3/less/bootstrap.less";
+import {Card,Badge,ListGroup} from 'bootstrap3'
+  class App extends Component {
+      constructor(props) {
+        super(props);
+ this.state = {
       hideCompleted: false,
       check:true,
       nav:true,
@@ -36,31 +34,29 @@ class App extends Component {
  
   handleSubmit() {
    
-    //const taskId = this.state.content._id
-    // Find the text field via the React ref
-    const text = this.state.title;
-    const text1 =this.state.contentData;
-    const cmnt = this.state.commentsArray
-this.state.edit?Meteor.call('tasks.update',text,text1,this.state.content._id):
-    Meteor.call('tasks.insert', text,text1,cmnt);
-    Tasks.insert({
-      text,
-      text1,
-      cmnt,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),           // _id of logged in user
-      username: Meteor.user().username,  // username of logged in user
-    });
+      const text = this.state.title;
+      const text1 =this.state.contentData;
+      const cmnt = this.state.commentsArray
+      this.state.edit?Meteor.call('tasks.update',text,text1,this.state.content._id):
+      Meteor.call('tasks.insert', text,text1,cmnt);
+      Tasks.insert({
+          text,
+          text1,
+          cmnt,
+          createdAt: new Date(), // current time
+          owner: Meteor.userId(),           // _id of logged in user
+          username: Meteor.user().username,  // username of logged in user
+        });
  
-    // Clear form
-    this.setState({edit:false})
-   this.setState({title:''})
-   this.setState({contentData:''})
-  }
-  toggleHideCompleted() {
-    this.setState({
-      hideCompleted: !this.state.hideCompleted,
-    });
+        // Clear form
+        this.setState({edit:false})
+        this.setState({title:''})
+        this.setState({contentData:''})
+        }
+        toggleHideCompleted() {
+            this.setState({
+            hideCompleted: !this.state.hideCompleted,
+          });
     
   }
   AddComment(){
@@ -74,7 +70,7 @@ this.state.edit?Meteor.call('tasks.update',text,text1,this.state.content._id):
     this.setState({commentsArray: task.cmnt})
   }
   deleteCmnt(index){
-    const taskId = this.state.content._id
+      const taskId = this.state.content._id
       Meteor.call('tasks.removeCmnt',index,taskId);
       Meteor.subscribe('tasks').ready()
       const task = Tasks.findOne(taskId);
@@ -93,238 +89,219 @@ this.state.edit?Meteor.call('tasks.update',text,text1,this.state.content._id):
     this.setState({activePage: pageNumber});
   }
   renderTasks() {
-  
-    let filteredTasks = this.props.tasks;
-    if (this.state.hideCompleted) {
-      filteredTasks = filteredTasks.filter(task => !task.checked);
-    }
-    return filteredTasks.slice(this.state.start,this.state.end).map((task) => {
-      const currentUserId = this.props.currentUser && this.props.currentUser._id;
-      const showPrivateButton = task.owner === currentUserId;
-       
-      return (
+      let filteredTasks = this.props.tasks;
+      if (this.state.hideCompleted) {
+          filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+      return filteredTasks.slice(this.state.start,this.state.end).map((task) => 
+        {
+            const currentUserId = this.props.currentUser && this.props.currentUser._id;
+            const showPrivateButton = task.owner === currentUserId;
+       return (
         <Task
           key={task._id}
           task={task}
           showPrivateButton={showPrivateButton}
-          editPress={(t)=>{this.setState({edit:true}), this.setState({content:t}),this.setState({check:false}),this.setState({title:t.text}),this.setState({contentData:t.text1})}}
-          onPressBtn={(t)=>{this.setState({content:t}),this.props.currentUser ?this.setState({nav:false}):alert("Please Log in first"),this.setState({commentsArray: t.cmnt});
+          editPress={(t)=>{
+            this.setState({edit:true}),
+            this.setState({content:t}),
+            this.setState({check:false}),
+            this.setState({title:t.text}),
+            this.setState({contentData:t.text1})
+          }}
+          onPressBtn={(t)=>{
+            this.setState({content:t}),
+            this.props.currentUser ?this.setState({nav:false}):
+             alert("Please Log in first"),
+            this.setState({commentsArray: t.cmnt});
           }}
         />
       );
     });
   }
   handleChange (text) {
-  //  this.setState({edit:false})
     this.setState({title:text.target.value})
-
   }
   handleChange1 (text) 
   {
-    //  this.setState({edit:false})
-      this.setState({contentData:text.target.value})
-  
-    }
-    handleChange2 (text) 
-  {
-    //  this.setState({edit:false})
-      this.setState({Comment:text.target.value})
-  
-    }
-   
-  render() {
-    
-  const hideFirstLastPages = `render() {
-  return (<Pagination
-    activePage={this.state.activePage}
-    itemsCountPerPage={8}
-    totalItemsCount={this.props.tasks.length}
-    pageRangeDisplayed={this.props.tasks.length>=2?2:1}
-    onChange={(pageNumber)=>this.handlePageChange(pageNumber)}
-  />)}`
- return (
-  this.state.nav? this.state.check?<div className="container">
-       <header style={{justifyContent:'space-between',height:'20%'}}>
-        <UIWrapper /> 
-        <h1>List of blog-Post ({this.props.incompleteCount})</h1>
-          <label className="hide-completed">
-            <button
-          onClick={()=>{ this.props.currentUser ?this.setState({check:false}):alert("Please Log in first")}}
-            >Write a blog</button>
-            
-          </label>
-         
-          {/* { this.props.currentUser ?
-            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-              <input
-                type="text"
-                ref="textInput"
-                placeholder="Type to add new tasks"
-              />
-            </form>
-             : ''
-          } */}
-         
-        </header>
- 
-        <ul>
-        {this.renderTasks()}
-        </ul>
-        <header>{this.props.tasks.length>8?<div className="panel-body">
-<div className='text-center'><Pagination
-          activePage={this.state.activePage}
-          itemsCountPerPage={8}
-          firstPageText={1}
-          lastPageText={this.props.tasks.length%8==0?this.props.tasks.length/8:parseInt(this.props.tasks.length/8)+1}
-          totalItemsCount={this.props.tasks.length}
-          pageRangeDisplayed={this.props.tasks.length>=2?2:1}
-          onChange={(pageNumber)=>this.handlePageChange(pageNumber)}
-        /></div></div>:''}</header>
- </div>:<div className="container">
-          <header>
-        <UIWrapper /> 
-          <label className="hide-completed">
-            <button
-          onClick={()=>this.setState({check:true})}
-            >Cancel</button>
-             
-          </label>
-          <label className="hide-completed">   <button
-          onClick={()=> this.state.title==""?'':this.handleSubmit()}
-            >Save</button></label>
-         <Posts/>
-         <label className="post-Title">
-         <h3>Title:</h3>
-              <input className="textTitle"
-                type="text"
-                id="textInput"
-                ref="textInput"
-                value={this.state.title}
-                onChange={(text)=> {this.handleChange(text)}
-                }
-                placeholder="Add a title for your Post"
-              />
-           
-            </label>
-            { this.props.currentUser ?'':this.setState({check:true})}
-        </header>
-     <div className='Body'>
-     <label className="post-Title"><h2>Containt:</h2></label><textarea className="textContent"
-      type="text"
-      ref="Content"
-      value={this.state.contentData}
-      onChange={(text)=> {this.handleChange1(text)}}
-      placeholder="Blog Content"/>
-          </div>
-          
-
-
-      </div>:<div className="container">
-          <header>
-       
-          <label className="hide-completed">
-            <button
-          onClick={()=>{this.setState({nav:true}),this.setState({commentsArray:[]})}}
-            >Back</button>
-             
-          </label> {/*
-          <label className="hide-completed">   <button
-          onClick={()=>ReactDOM.findDOMNode(this.refs.textInput).value==""?'':this.handleSubmit()}
-            >Save</button></label> */}
-          <label className="post-Title">
-
-        <h3>Writer: {this.state.content.username}</h3></label>
-      
-         <label className="post-Title">
-         <h3>{this.state.content.text}</h3>
-              {/* <input className="textTitle"
-                type="text"
-                ref="textInput"
-                placeholder="Add a title for your Post"
-              /> */}
-           
-            </label>
-            { this.props.currentUser ?'':this.setState({check:true})}
-        </header>
-     <div className='Body'>
-     <label className="post-Title"></label><div className="textShow"
-    >{this.state.content.text1}</div>
-          </div>
-          
-          <div className='Comments'>
-     <label className="Comments"><h3>Comments:</h3></label>{
-      this.state.commentsArray.map((item,index)=>
-    {
-      var a = JSON.parse(item)
-
-      
-    return( <header>
-       <div  className='Comments'><strong> {JSON.stringify(a.createdAt).slice(1,11)} {a.username} : </strong>{a.array}</div>
-       { Meteor.user().username == a.username ?<button className="delete" onClick={()=>this.updateCmnt(index,a.array)}>
-         edit
-        </button>:''
-        
- } { Meteor.user().username == a.username ?<button className="delete" onClick={()=>this.deleteCmnt(index)}>
-         Delete
-        </button>:''
-        
- }
-    </header>)
-    })
-    }
-
-   
-          </div>
-  <header>
-       
-       {/*
-       <label className="hide-completed">   <button
-       onClick={()=>ReactDOM.findDOMNode(this.refs.textInput).value==""?'':this.handleSubmit()}
-         >Save</button></label> */}
-       
-
-     <h3>{Meteor.user().username}</h3><textarea className="Comments"
-      type="text"
-      ref="Comment"
-      placeholder="Comment"
-      value={this.state.Comment}
-      onChange={(text)=> {this.handleChange2(text)}}/>
-        
-      <label className="hide-completed">
-            <button
-          onClick={()=>this.setState({Comment:""})}
-            >Clear</button>
-             
-          </label>
-    <label className="hide-completed">
-         <button
-       onClick={()=>{this.AddComment(),this.setState({Comment:""})}}
-         >Add Comment</button>
-          
-       </label>
-     
-      <label className="post-Title">
-      <h3></h3>
-           {/* <input className="textTitle"
-             type="text"
-             ref="textInput"
-             placeholder="Add a title for your Post"
-           /> */}
-        
-         </label>
-         { this.props.currentUser ?'':this.setState({check:true})}
-       
-     </header>
-      </div>
-     );
-    }
+    this.setState({contentData:text.target.value})
   }
-   
-  export default withTracker(() => {
+  handleChange2 (text) 
+  {
+    this.setState({Comment:text.target.value})
+  }
+  render() {
+      const hideFirstLastPages = `render() {
+        return (<Pagination
+        activePage={this.state.activePage}
+        itemsCountPerPage={8}
+        totalItemsCount={this.props.tasks.length}
+        pageRangeDisplayed={this.props.tasks.length>=2?2:1}
+        onChange={(pageNumber)=>this.handlePageChange(pageNumber)}
+      />)}`
+    return (
+      this.state.nav? this.state.check?<div className="container">
+          <header style={{justifyContent:'space-between',height:'20%'}}>
+            <UIWrapper /> 
+            <h1>Post Management Tool</h1>
+            <label className="hide-completed">
+              <button type="button" className="btn btn-secondary"
+                onClick={()=>{ this.props.currentUser ?this.setState({check:false}):alert("Please Log in first")}}
+                >Write a blog
+              </button>
+            </label>
+         </header>
+        <h2>
+          <span className="badge badge-secondary">
+              <h5>List of blog-Post ({this.props.incompleteCount})</h5>
+          </span>
+        </h2>
+        <ul className="list-group">
+            {this.renderTasks()}
+        </ul>
+        <header>{this.props.tasks.length>8?
+            <div className="panel-body">
+                <div className='text-center'>
+                  <Pagination
+                    activePage={this.state.activePage}
+                    itemsCountPerPage={8}
+                    totalItemsCount={this.props.tasks.length}
+                    pageRangeDisplayed={this.props.tasks.length>=2?2:1}
+                    onChange={(pageNumber)=>this.handlePageChange(pageNumber)}
+                  />
+                </div>
+              </div>:''}
+        </header>
+  </div>:
+    <div className="container">
+        <header>
+          <UIWrapper/> 
+            <label className="hide-completed">
+              <button
+                onClick={()=>this.setState({check:true})}
+                >Cancel
+              </button>
+            </label>
+            <label className="hide-completed">
+              <button
+                  onClick={()=> this.state.title==""?'':this.handleSubmit()}
+                  >Save
+              </button>
+            </label>
+            <label className="post-Title">
+                <h3>Title:</h3>
+                <input className="textTitle"
+                  type="text"
+                  id="textInput"
+                  ref="textInput"
+                  value={this.state.title}
+                  onChange={(text)=> {this.handleChange(text)}}
+                  placeholder="Add a title for your Post"
+                />
+           </label>
+          { this.props.currentUser ?'':this.setState({check:true})}
+        </header>
+      <div className='Body'>
+        <label className="post-Title">
+          <h2>Containt:</h2>
+        </label>
+        <textarea className="textContent"
+          type="text"
+          ref="Content"
+          value={this.state.contentData}
+          onChange={(text)=> {this.handleChange1(text)}}
+          placeholder="Blog Content"/>
+      </div>
+  </div>:<div className="container">
+           <header>
+              <label className="hide-completed">
+                  <button
+                    onClick={()=>{
+                      this.setState({nav:true}),
+                      this.setState({commentsArray:[]})
+                    }}
+                    >Back
+                  </button>
+              </label> 
+              <label className="post-Title">
+                <h3>Writer: {this.state.content.username}</h3>
+               </label>
+              { this.props.currentUser ?'':this.setState({check:true})}
+            </header>
+            <div className='Body'>
+                <div className="card" >
+                  <div className="card-body">
+                    <h3 className="card-title">
+                      {this.state.content.text}
+                    </h3>
+                     <p className="card-text">{this.state.content.text1}</p>
+                   </div>
+                </div>
+          </div>
+         <div className='Comments'>
+         <label className="Comments">
+           <h3>Comments:</h3>
+         </label>{
+            this.state.commentsArray.map((item,index)=>
+               {
+                    var a = JSON.parse(item)
+                    return( <header>
+                          <div  className='Comments'>
+                            <strong> {JSON.stringify(a.createdAt).slice(1,11)} {a.username} :
+                           </strong>{a.array}</div>
+                          { Meteor.user().username == a.username ?
+                            <button className="delete" 
+                              onClick={()=>this.updateCmnt(index,a.array)}>
+                                  edit
+                            </button>:''
+                          }
+                          { Meteor.user().username == a.username ?
+                              <button className="delete" 
+                                  onClick={()=>this.deleteCmnt(index)}>
+                                      Delete
+                              </button>:''
+                          }
+                      </header>)
+              })
+            }
+        </div>
+        <header>
+            <h3>{Meteor.user().username}</h3>
+            <textarea 
+              className="Comments"
+              type="text"
+              ref="Comment"
+              placeholder="Comment"
+              value={this.state.Comment}
+              onChange={(text)=> {
+                      this.handleChange2(text)
+                      }}
+            />
+       <label className="hide-completed">
+          <button type="button" className="btn btn-secondary"
+              onClick={()=>this.setState({Comment:""})}
+            >Clear</button>
+       </label>
+       <label className="hide-completed">
+         <button type="button" className="btn btn-secondary"
+            onClick={()=>{
+              this.AddComment(),
+              this.setState({Comment:""})
+              }}
+          >Add Comment</button>
+      </label>
+      <label className="post-Title">
+      </label>
+         { this.props.currentUser ?'':this.setState({check:true})}
+       </header>
+  </div>);
+  }
+}
+export default withTracker(() => {
     Meteor.subscribe('tasks');
     return {
       tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
       incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
       currentUser: Meteor.user(),
     };
-  })(App);
+})(App);
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -3,53 +3,44 @@ import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../api/tasks.js';
 import classnames from 'classnames';
 import Pagination from "react-js-pagination";
-// Task component - represents a single todo item
+import {Buttons} from 'bootstrap3'
 export default class Task extends Component {
-  toggleChecked() {
-    // Set the checked property to the opposite of its current value
-    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
-  }
  
-  deleteThisTask() {
-    Meteor.call('tasks.remove', this.props.task._id);
-  }
-  togglePrivate() {
-    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
-  }
-  render() {
-    // Give tasks a different className when they are checked off,
-    // so that we can style them nicely in CSS
-    const taskClassName = classnames({
-        checked: this.props.task.checked,
-        private: this.props.task.private,
-      });
- 
-    return (
-          <li className={taskClassName}>
-           { this.props.showPrivateButton ?<button className="delete" onClick={()=>this.props.editPress(this.props.task)}>
-         Edit
-        </button>:''
- }
-       { this.props.showPrivateButton ?<button className="delete" onClick={this.deleteThisTask.bind(this)}>
-         Delete
-        </button>:''
- }
-        {/* <input
-          type="checkbox"
-          readOnly
-          checked={!!this.props.task.checked}
-          onClick={this.toggleChecked.bind(this)}
-        /> */}
- {/* { this.props.showPrivateButton ? (
-          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
-            { this.props.task.private ? 'Private' : 'Public' }
-          </button>
-        ) : ''} */}
-
- <button className="toggle-private" onClick={()=>this.props.onPressBtn(this.props.task)}><span className="text">
- <strong>{JSON.stringify(this.props.task.createdAt).slice(1,11)}:- {this.props.task.username}</strong>: {this.props.task.text}
-        </span></button>
-      </li>
+    toggleChecked() {
+      Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
+    }
+    deleteThisTask() {
+      Meteor.call('tasks.remove', this.props.task._id);
+    }
+    togglePrivate() {
+      Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+    }
+    render() {
+        const taskClassName = classnames({
+            checked: this.props.task.checked,
+            private: this.props.task.private,
+            });
+        return (
+           <li className="list-group-item list-group-item-light">
+              { 
+                this.props.showPrivateButton ?<button className="delete" 
+                onClick={()=>
+                           this.props.editPress(this.props.task)
+                       }>Edit</button>:''
+              }          
+              { 
+                this.props.showPrivateButton ?<button className="delete" 
+                onClick={
+                         this.deleteThisTask.bind(this)
+                      }>Delete</button>:''
+              }
+              <button type="button" className="btn btn-secondary"  onClick={()=>this.props.onPressBtn(this.props.task)}>
+                  <span className="text">
+                    <strong>{JSON.stringify(this.props.task.createdAt).slice(1,11)}:- {this.props.task.username}
+                    </strong>:{this.props.task.text}
+                  </span>
+              </button>
+            </li>
     );
   }
 }
